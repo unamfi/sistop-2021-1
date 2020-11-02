4b384680ae1896dd9fc01cc6c14e2f2a0a5b4129
Carol-Guez <carol@pc-crd.fi.unam>
2020-11-01 19:57:39 -0600

    Creación del hilo profe

--- tareas/1/MontielJavier-RodriguezCarolina/profe_alumnos.py
+++ tareas/1/MontielJavier-RodriguezCarolina/profe_alumnos.py
@@ -3,20 +3,27 @@ import random
 import time 
 
 def alumnos(id):
-    #while True:
-    print('Soy el alumno %d ' %id)
+  profe_dormido.release()  
+        
 
-    sillas.acquire()
-    print('   Se sento el alumno %d' %id)
-    #profe.acquire()
-    print('***El alumno %d deja la silla' %id)
-    sillas.release()
+def profe():
+    while True:
+        if len(alumnos_en_silla) > 0:
+            print('Resolviendo duda')
+            time.sleep(random.random())
+            alumno_id = alumnos_con_preguntas.pop()
+            print('Duda resuelta del alumno %d' %alumno_id)
+     
+        else:
+            profe_dormido.acquire()
+            print('Profesor descansando')
 
 num_alumno = 20
+alumnos_con_preguntas = []
+alumnos_en_silla = []
 sillas = threading.Semaphore(2)
-profe = threading.Semaphore(1)
+profe_dormido = threading.Semaphore(0)
 
+threading.Thread(target=profe).start()
 for alumno_id in range(num_alumno):
     threading.Thread(target=alumnos,args=[alumno_id]).start()
-
-#def preguntar()
