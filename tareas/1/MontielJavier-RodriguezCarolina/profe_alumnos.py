d2b67830d4128d499aa7f41425de3e65c8463dd6
Carol-Guez <carol@pc-crd.fi.unam>
2020-11-01 21:22:28 -0600

    Version final del programa, se agrego un num de preguntas aleatorio

--- tareas/1/MontielJavier-RodriguezCarolina/profe_alumnos.py
+++ tareas/1/MontielJavier-RodriguezCarolina/profe_alumnos.py
@@ -5,7 +5,8 @@ import random
 import time 
 
 def alumnos(id):
-    while True:
+    num_preguntas = random.randint(1, 10)
+    while num_preguntas > 0:
         sillas.acquire()
         print('---->El alumno %d consiguió silla' %id)
         alumnos_en_silla.append(id)
@@ -13,7 +14,8 @@ def alumnos(id):
         if len(alumnos_en_silla) == 1:
             profe_dormido.release()
         mutex_primer_alumno.release()
-
+        num_preguntas= num_preguntas - 1
+    print('------->El alumno %d ya NO tiene dudas, ya se va' %id)
         
 
 def profe():
@@ -24,15 +26,16 @@ def profe():
             alumno_id = alumnos_en_silla.pop()
             print('->Duda resuelta del alumno %d' %alumno_id)
             sillas.release()
-     
+            print('El alumno %d dejo la silla' %alumno_id)
         else:
             print('->Profesor descansando')
             profe_dormido.acquire()
             print('->Profesor despierto')
 
-num_alumno = 20
+num_alumno = 10
+num_sillas = 2
 alumnos_en_silla = []
-sillas = threading.Semaphore(2)
+sillas = threading.Semaphore(num_sillas)
 profe_dormido = threading.Semaphore(0)
 mutex_primer_alumno = threading.Semaphore(1)
 
