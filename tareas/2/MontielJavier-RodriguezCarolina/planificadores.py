55fed4363d15fa8269d99add5f05abc70e349bc4
luisjaviermon <eleloteesdeliciosa@gmail.com>
2020-11-18 04:52:31 -0600

    diseñando la logica del planificador

--- tareas/2/MontielJavier-RodriguezCarolina/planificadores.py
+++ tareas/2/MontielJavier-RodriguezCarolina/planificadores.py
@@ -1,20 +1,49 @@
-from timeit import default_timer
-import threading
-import time
+"""
+------------->Responsabilidades
+===>Tiempo
++el lleva la cuenta del tiempo general
++el avisa a cada proceso cuando iniciar
++saber el tiempo de finalizacion del proceso
 
-def carga(id_proceso,tiempo_duracion):
-    tiempo_inicial = default_timer()
-    tiempo_en_ejecucion = 0
-    tiempo_final = 0
+-proceso
++solicitar cpu
++detener ejecucion 
 
-    print('iniciando ciclo...')
-    
-    while(tiempo_en_ejecucion < tiempo_duracion):
-        tiempo_final = default_timer()
-        tiempo_en_ejecucion = tiempo_final - tiempo_inicial
-    
-    print('Fin del proceso',id_proceso)
-    print("Tiempo total", tiempo_en_ejecucion, "segundos")
+-planificador
++avisar cuando iniciar
++avisar cuando detenerse 
 
-hilo_prueba = threading.Thread(target = carga,args = []) 
 
+------------->metodos
+=====> Tiempo
+*llamar: Avisa al hilo que sebe de solicitar algo(ya sea pedir CPU o avisar que finaliza)
+-tiene 3 colas ordenadas 
+una para almacenar q inicio
+una para almacenar q fin
+una para almacenar a que proceso pertenece
+
+======>Planificador
+*respuesta: avisar a proceso que lo llamo si puede o no usar CPU
+*checarquantums: ver si cada x q debe de verificar disponibilidad de CPU
+-proceso actual: espacio para almacenar que proceso esta en ejecucion
+-procesos pendientes: cola para saber el proximo proceso a ejecutar
+*colocar proceso actual en ejec
+*quitar al proc actual en ejec
+*colocar proceso en espera
+*quitar proceso en espera
+"""
+
+"""
+OBJETIVO GENERAL: DEFINIR LA LOGICA DE CADA COSA
+
+objetivo presente: ¿Quien debe empezar?
+
+PENDIETES 
+-REVISAR LA LOGICA ACTUAL PARA SPN(,FB,SRR)
+-METER CODIGO
+-VER COMO OBTENER TABLA DE PROMEDIOS
+-ESQUEMA VISUAL
+
+***
+
+"""
