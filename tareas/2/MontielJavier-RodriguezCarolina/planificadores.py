f70a737af6d0587455d509baffd183e1136cd530
luisjaviermon <eleloteesdeliciosa@gmail.com>
2020-11-18 23:45:02 -0600

    implementando objetos base

--- tareas/2/MontielJavier-RodriguezCarolina/planificadores.py
+++ tareas/2/MontielJavier-RodriguezCarolina/planificadores.py
@@ -1,28 +1,98 @@
 from timeit import default_timer
-ESTADO_LISTO = 1
-ESTADO_EN_EJECUCION = 0
-ESTADO_EN_BLOQUEO = 1
-DURACION_QUANTUM = 0.001
+DURACION_QUANTUM = 1
 CPU_CONCEDIDO = 1
 CPU_NEGADO = 0
+ESTADO_LISTO = 1
+ESTADO_FINALIZADO = 0
 
 class Tiempo:
     def __init__(self, procesos):
         self.procesos = procesos
-        self.procesos.sorted(self.procesos, key = lambda x : x.quantum_inicio)
-    
+        self.procesos = sorted(self.procesos, key = lambda x : x.quantum_inicio,reverse=False)
+        procesos = self.procesos
+
+    def corre_tiempo(self):
+        tiempo_cero = default_timer()
+        tiempo_actual = 0
+        
+        while tiempo_actual < :
+            tiempo_actual = default_timer()
+            tiempo_actual -= tiempo_cero
+            tiempo_actual = round(tiempo_actual,0)
+
+        print(tiempo_actual)
+
 class Proceso:
     def __init__(self, quantum_inicio, quantum_duracion, nombre_proceso):
         self.estado = ESTADO_LISTO
         self.quantum_duracion = quantum_duracion * DURACION_QUANTUM
         self.quantum_inicio = quantum_inicio * DURACION_QUANTUM
         self.nombre_proceso = nombre_proceso
+        self.tiempo_respuesta = 0
+        self.tiempo_espera = 0
+        self.proporcion_penalizacion = 0
 
     def set_tipo_aviso(self, estado):
         self.estado = estado
     
-    def solicitar_cpu():
-        if planificador.respuesta() == CPU_CONCEDIDO:
+    def solicitar_cpu(self):
+        tiempo_actual = 0
+        tiempo_anterior = 0
+        tiempo_en_ejecucion = 0
+        tiempo_bloqueado = 0
+
+        print('inicia el ciclo...')
+
+
+        tiempo_anterior = default_timer()
+        while tiempo_en_ejecucion < self.quantum_duracion:
+
+            tiempo_actual = default_timer()
+
+            if planificador.respuesta(self.nombre_proceso) == CPU_CONCEDIDO:
+                tiempo_en_ejecucion += tiempo_actual-tiempo_anterior
+            else:
+                tiempo_bloqueado += tiempo_actual-tiempo_anterior
+            
+            tiempo_anterior = tiempo_actual
+
+        self.set_tipo_aviso(ESTADO_FINALIZADO)
+
+class Planificador:
+    def __init__(self,procesos):
+        self.procesos = procesos
+        self.procesos_en_espera = []
+        self.proceso_actual = None 
+
+    def respuesta(self,nombre_proceso):
+        if self.proceso_actual == None or self.proceso_actual == nombre_proceso:
+            return CPU_CONCEDIDO
+        else:
+            return CPU_NEGADO
+    
+    def ejecutando_proceso(self,nombre_proceso):
+        self.proceso_actual = nombre_proceso
+
+    def detener_proceso(self,nombre_proceso):
+        self.proceso_actual = None
+
+    def FCFS(self):
+        procesos = self.procesos[:]
+        tiempo = Tiempo(procesos)
+        tiempo.corre_tiempo()
+        
+        
+procesos = [] #1,0
+B = Proceso(1,5,'B')
+procesos.append(B)
+A = Proceso(0,3,'A')
+procesos.append(A)
+C = Proceso(3,2,'C')
+procesos.append(C)
+planificador = Planificador(procesos)
+planificador.FCFS()
+
+        
 
     
     # def get_tipo_aviso(self):
