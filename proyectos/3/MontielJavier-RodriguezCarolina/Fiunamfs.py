382f68b0cee56be8d85800a1ec03cd9e7c28165c
luisjaviermon <eleloteesdeliciosa@gmail.com>
2020-12-30 13:01:09 -0600

    refactorizando ls

--- proyectos/3/MontielJavier-RodriguezCarolina/Fiunamfs.py
+++ proyectos/3/MontielJavier-RodriguezCarolina/Fiunamfs.py
@@ -38,14 +38,85 @@ class Fiunamfs:
       print('Con este disco no puedo trabajar')
       self.sistema_archivo.close()
 
-  def ls(self):
-    
+  def listar_archivos(self, metadatos = False):
+    archivos = []
+
     for i in range(0,64):
-      self.sistema_archivo.seek(self.tamanio_cluster+(i*64))
+      posicion = self.tamanio_cluster+(i*64)
+      self.sistema_archivo.seek(posicion)
       nombre_archivo = self.sistema_archivo.read(14)
 
       if nombre_archivo != b'Xx.xXx.xXx.xXx':
-        print("[",i+1,"]"," ",nombre_archivo.decode())
+        if metadatos:
+          archivo = []
+          self.sistema_archivo.seek(posicion+16)
+          archivo.append(self.a_32.unpack(self.sistema_archivo.read(4))[0])
+          self.sistema_archivo.seek(posicion+20)
+          archivo.append(self.a_32.unpack(self.sistema_archivo.read(4))[0])
+          archivos.append(archivo)
+        else:
+          archivos.append(nombre_archivo)
+
+    return archivos
+    
+
+  def ls(self):
+    lista_archivos = self.listar_archivos()
+
+    for i in lista_archivos:
+      print("-> ",i.decode())
+      
+  #cp origen destino
+  #cp -l (del sistema al disckete) ar1 ar2
+  #cp ar1 ar2 (del disckete al sist)
+
+
+
+    '''
+    listar()
+      -> recorrer todo el directorio devilver un arreglo con los nombres
+
+    ls()
+      ->manda a llamar a listar()
+      ->imprima el arreglo retornado
+
+    existe_archivo(listado, nombre)
+      ->recorre la lista comparando con el nombre ingresado
+
+
+    copiar(nombre)
+      llama a listar(nombre)
+      si existe_archivo(lista,nombre)
+        creamos archivo
+        hacemos copiado
+      ecc
+        manda mensaje
+      
+    '''
+
+    '''
+    --copiar de fi al sistema 
+    -> si encuentra el archivo
+      -> sacar su info (tamaño y cluster inicial)
+      -> crear archivo en la ruta especificada
+      -> posicionar en el cluster inicial
+      -> leer todo el archivo
+      -> escribir todo el archivo en la ruta esp
+      -> cerrar el archivo copiado
+    ->ecc
+      ->mensaje de error
+
+
+    '''
+
+    '''
+    --tentativo para desfragmentar
+    -> crear el bitmap para ver los espacios
+    -> recorrer el bitmap
+      -> en caso de encontrar un hueco 
+        -> analizar tamaño del hueco 
+        -> recorrer el siguiente bloque de informacion al espacio hueco
+    '''
 
 
 if __name__ == '__main__':
