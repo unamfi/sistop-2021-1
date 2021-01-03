e48b105b0eb3515e790ea9509299f4307aa82930
Carol-Guez <carol@pc-crd.fi.unam>
2021-01-02 22:22:55 -0600

    Terminamos implementación de copiado de sistema hacía fiunamfs.img

--- proyectos/3/MontielJavier-RodriguezCarolina/Fiunamfs.py
+++ proyectos/3/MontielJavier-RodriguezCarolina/Fiunamfs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3 
-import os, struct, time, datetime
+import os, struct, time, datetime, math
 
 class Fiunamfs:
   def __init__(self):
@@ -86,38 +86,84 @@ class Fiunamfs:
     return info_archivo
 
   def verifica_espacio_disponible(self,tamanio_archivo):
-    archivos_almacenados = self.listar_archivos()
-    if len(archivos_almacenados) > 0:
-      return True
+    archivos_almacenados = self.listar_archivos(True)
     tamanio_ocupado = 0
-    for i in archivos_almacenados:
-      tamanio_ocupado += i[1]
+    if len(archivos_almacenados) > 0:
+      for i in archivos_almacenados:
+        tamanio_ocupado += i[1]
+    
     tamanio_disponible = 1474560 - tamanio_ocupado - 5120
 
     return True if tamanio_disponible > tamanio_archivo else False
 
-    
+  
+  def ordenar_archivos(self,lista_archivos,elemento_criterio):
+    return sorted(lista_archivos, key=lambda dato: dato[elemento_criterio])
+
   def asigna_cluster_inicial(self,tamanio_archivo):
-    '''
-    -> listamos todos los archivos 
-    -> vemos si podemos ordenar todo en base al cluster de inicio
-    -> analizamos si el cluster actual y el proximo es el continuo (para todo el arreglo) (si el cluster es el 12 y pesa 3 clusters, termina 
-    en el 15, por lo tanto el proximo archivo inicie en el cluster 16)
-      -> en caso de que no vemos cuanto espacio hay
-      -> analizamos si el espacio es suficiente para almacenar el archivo 
-      -> si el tamaño es suficiente lo escribimos tanto en el directorio como en el espacio disponible 
-      ->en caso contrario seguimos 
-    ->  si no se logro encontrar hueco entre clusters vemos si hay espacio desde el ultimo cluster
-    hasta el final del espacio disponible en disco 
-    -> en caso de haber espacio almacenamos el archivo al final 
-    ''' 
+    archivos = self.listar_archivos(True)
+    archivos = self.ordenar_archivos(archivos,2)
+    for i in range (0,len(archivos)-1):
+      #print("actual ",archivos[i],"\nsiguiente ",archivos[i+1])
+      tamanio_en_clusters = math.floor(archivos[i][1]/1024)
+      cluster_final_actual = archivos[i][2] + tamanio_en_clusters
+      tamanio_en_clusters = math.floor(archivos[i+1][1]/1024)
+      cluster_final_siguiente = archivos[i+1][2] + tamanio_en_clusters
+      espacio_entre_bloques = (cluster_final_siguiente - cluster_final_actual) - 1
+      if espacio_entre_bloques >= math.floor(tamanio_archivo/1024):
+        return cluster_final_actual + 1
+    tamanio_en_clusters = math.floor(archivos[-1][1]/1024)
+    cluster_final_actual = archivos[-1][2] + tamanio_en_clusters
+    tamanio_restante = (1440 - cluster_final_actual)*1024
+    
+    if tamanio_restante >= tamanio_archivo:
+      return cluster_final_actual + 1
+    
+    return -1
+
+  def escribe_archivo(self,ruta_sistema,info_archivo,cluster_inicial):
+    nombre = ruta_sistema.split('/')
+    for i in range(0,64):
+      posicion = self.tamanio_cluster+(i*64)
+      self.sistema_archivo.seek(posicion)
+      nombre_archivo = self.sistema_archivo.read(14)
+      if nombre_archivo == b'Xx.xXx.xXx.xXx':
+        print("si encontro")
+        posicion = self.tamanio_cluster+(i*64)
+        self.sistema_archivo.seek(posicion)
+        self.sistema_archivo.write(nombre[-1].encode())
+
+        self.sistema_archivo.seek(posicion+15)
+        self.sistema_archivo.write(self.a_32.pack(info_archivo[0]))
+
+        self.sistema_archivo.seek(posicion+16)
+        self.sistema_archivo.write(self.a_32.pack(info_archivo[0]))
+
+        self.sistema_archivo.seek(posicion+20)
+        self.sistema_archivo.write(self.a_32.pack(cluster_inicial))
+
+        self.sistema_archivo.seek(posicion+24)
+        self.sistema_archivo.write(time.strftime("%Y%m%d%I%M%S").encode())
+
+        self.sistema_archivo.seek(posicion+38)
+        self.sistema_archivo.write(info_archivo[1].encode())
+        break
+
+    archivo_origen = open(ruta_sistema,"rb")
+    contenido = archivo_origen.read(info_archivo[0])
+    self.sistema_archivo.seek(cluster_inicial*1024)
+    self.sistema_archivo.write(contenido)
+    archivo_origen.close()
 
   def copiar_a_fiunamfs(self,ruta_sistema,ruta_disqute):
-    if self.existe_archivo_en_sistema(ruta_sistema):
+    if self.existe_archivo_en_sistema(ruta_sistema): 
       info_archivo = self.obtener_info_archivo(ruta_sistema)
-      
       if self.verifica_espacio_disponible(info_archivo[0]):
-        print("si hay espacio disponible")
+        cluster_inicio = self.asigna_cluster_inicial(info_archivo[0])
+        if cluster_inicio != -1:
+          self.escribe_archivo(ruta_sistema,info_archivo,cluster_inicio)
+        else:
+          print("NO entro :(")
       else:
         print("no hay espacio disponible") 
       
@@ -171,8 +217,30 @@ class Fiunamfs:
         -> recorrer el siguiente bloque de informacion al espacio hueco
     '''
 
+    '''
+    -> listamos todos los archivos 
+    -> vemos si podemos ordenar todo en base al cluster de inicio
+    [[x,6],[x,5]] -> [[x,5],[x,6]]
+    -> analizamos si el cluster actual y el proximo es el continuo (para todo el arreglo) (si el cluster es el 12 y pesa 3 clusters, termina 
+    en el 15, por lo tanto el proximo archivo inicie en el cluster 16)
+      -> en caso de que no vemos cuanto espacio hay
+      -> analizamos si el espacio es suficiente para almacenar el archivo 
+      -> si el tamaño es suficiente lo escribimos tanto en el directorio como en el espacio disponible 
+      ->en caso contrario seguimos 
+    ->  si no se logro encontrar hueco entre clusters vemos si hay espacio desde el ultimo cluster
+    hasta el final del espacio disponible en disco 
+    -> en caso de haber espacio almacenamos el archivo al final 
+    '''
+
 
 if __name__ == '__main__':
   sistema = Fiunamfs()
-  #sistema.copiar_a_fiunamfs('/home/javier/Downloads/APUNTES_DE_ALGEBRA_LINEAL_LEDA.pdf','')
-  sistema.ls()
+  sistema.copiar_a_fiunamfs('/home/carol/Desktop/hola1.pdf','')
+  #sistema.ls()
+
+'''
+-> readme tam = 30751 c = 5
+-> logo tam = 0 c = 37
+-> datetime tam 63 c = 352
+-> hola tam 4000  c =  38
+'''
