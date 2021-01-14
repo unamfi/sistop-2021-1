2fd3d0d54efc83ffb84bcf9d2c28429a1dccabb3
Jon_Calz <jcalzadamtz@gmail.com>
2021-01-14 17:47:12 -0600

    Actualizado

--- proyectos/3/CalzadaJonathan/sistarchivos.py
+++ proyectos/3/CalzadaJonathan/sistarchivos.py
@@ -5,9 +5,10 @@ Created on Thu Jan 8 15:37:48 2021
 @author: Jonathan
 """
 
-# importamos las librerias nescesarias
-import os.path #para interactuar con el sistema operativo
-from math import ceil #Se utiliza ceil para asignar el valor del cluster
+import os.path # Se utiliza para asignar rutas en el sistema 
+from math import ceil#Se utiliza ceil para asignar el valor del cluster 
+
+
 
 #Cuerpo del programa
 if os.path.exists("fiunam.img"):# verificar si la ruta especificada existe
@@ -19,6 +20,7 @@ else:
 	file.seek(10)
 	file.write("0.4")
 	file.seek(20)
+	file.write("Sistema Archivo")
 	file.seek(40)
 	file.write("01024")
 	file.seek(47)
@@ -27,6 +29,7 @@ else:
 	file.write("00001440")
 	file.seek(1024)
 	for i in range(64):
+		file.write("AQUI_NO_VA_NADA")
 		file.seek(file.tell()+1)
 		file.write("00000000")
 		file.seek(file.tell()+1)
@@ -37,8 +40,9 @@ else:
 		file.write("00000000000000")
 		file.seek(file.tell()+4)
 	file.close()
+ 
 
-#Funcion para agregar archivo de la computadora al sistema de archivos nuevo
+#Funcion para agregar archivo de la computadora al sistema de archivos
 def agregar(nombre):
 	size=os.path.getsize(nombre)# os.path verificamos la ruta existente
 	cluster_ant=0
@@ -51,9 +55,13 @@ def agregar(nombre):
 	while aux != 'AQUI_NO_VA_NADA':
 		file.seek(file.tell()+49)
 		aux=file.read(15)
+	#Si es el primer archivo en directorio
 	if file.tell()==1039:#Se verifica si es el primer archivo dentro del sistema de archivos
 		cluster_act=4
+		ahora=file.tell()
 	else:
+		ahora=file.tell()
+		anterior=file.seek(file.tell()-63)
 		tamanio=file.read(8)
 		file.seek(file.tell()+1)
 		cluster_ant=file.read(5)
@@ -82,13 +90,13 @@ def agregar(nombre):
 		file.write(str(cluster_act+cluster))
 
 #Funcion que copia archivo del sistema a la computadora
-def copiar(nombre):
+def copy(nombre):
 	size=0
 	cluster=0
 	name=nombre
 	copia=""
 	while name==nombre:
-		print("Escribe el nombre del nuevo archivo")
+		print("Introduce el nombre del nuevo archivo")
 		name=input()
 	file=open("fiunam.img","r+",encoding="Latin-1")
 	file.seek(1024)
@@ -106,7 +114,7 @@ def copiar(nombre):
 	archivo.write(copia)
 	archivo.close()
 	file.close()
-	return(" ")
+	return("Archivo copiado con exito")
 
 #Muestra el contenido del FS
 def listar():
@@ -120,7 +128,7 @@ def listar():
 	file.close()
 
 #Esta funcion elimina del sistema de archivos
-def quitar(nombre):#Recibe el nombre del archivo a eliminar
+def eliminar(nombre):#Recibe el nombre del archivo a eliminar
 	file=open("fiUnam.img","r+")
 	file.seek(1024)
 	for i in range(64):
@@ -128,11 +136,12 @@ def quitar(nombre):#Recibe el nombre del archivo a eliminar
 		if archivoAux.replace(' ','') == nombre:
 			#Recupera informacion necesaria del archivo a eliminar
 			file.seek(file.tell()+1)
-			mag=int(file.read(8))
+			tam=int(file.read(8))
 			file.seek(file.tell()+1)
 			ini=int(file.read(5))
 			#Borra del directorio
 			file.seek(file.tell()-30)
+			file.write("AQUI_NO_VA_NADA")
 			file.seek(file.tell()+1)
 			file.write("00000000")
 			file.seek(file.tell()+1)
@@ -144,54 +153,50 @@ def quitar(nombre):#Recibe el nombre del archivo a eliminar
 			file.seek(file.tell()+4)
 			file.close()
 			#Limpia la memoria
-			limpiar(ini,ceil(mag/1024))#El tamaño se envia en clusters
+			limpiar(ini,ceil(tam/1024))#El tamaño se envia en clusters
 			return 'Se borro el archivo'
 		else:
 			file.seek(file.tell()+49)
 	file.close()
 	return 'No se encontro el archivo'
 
-
-
-
-#Esta funcion limpia la memoria con el inicio y la magnitud.
-def limpiar(ini,mag):
+#Esta funcion limpia la memoria con el inicio y la magnitud.(EStara en clusters)
+def limpiar(ini,tam):
 	file=open("fiUnam.img","r+")
 	file.seek(ini*1024)
 	vacio='\x00'
-	for i in range(0,mag):
+	for i in range(0,tam):
 		file.write(vacio*1024)
 	file.close()
-
 
 
 a=True
 while a==True:
    
-    print("_____________________________________ ")
-    print("|1. Listar contenido.				|")
-    print("|2. agregar archivo al Sist. de		|")
+    print("_____________________ ")
+    print("|1. Listar contenido.|")
+    print("|2. añadir archivo.|")
     print("|3. Copiar archivo a su computadora. |")
-    print("|4. Eliminar archivo.  			    |")#CHECK
-    print("|5. Salir.              				|")
-    print("| Seleccione un opcion: 				|")
-    print("_____________________________________-")
+    print("|4. Eliminar archivo.   |")#CHECK
+    print("|5. Salir.              |")
+    print("| Seleccione un opcion: |")
+    print("                    ")
     op=int(input("Ingrese una opcion: \n"))
     if op == 1:
         listar()
     elif op == 2:
         print("añadir archivo")
-        arg=input("Ingrese el nombre del archivo: ")
+        arg=input("Ingrese el nombre del archivo :")
         agregar(arg)
         print("Añadido")
     elif op == 3:
-        print("Copiar del FiUnamFs a la computadora:  ")
-        arg=input("Ingrese el nombre del archivo:  ")
-        copiar(arg)
+        print("Copiar del FiUnamFs a la computadora")
+        arg=input("Ingrese el nombre del archivo :")
+        copy(arg)
         print("copiado")
     elif op == 4:
-        arg=input("Ingrese el nombre del archivo:  ")
-        quitar(arg)
+        arg=input("Ingrese el nombre del archivo :")
+        eliminar(arg)
         print("Eliminado")
     elif op == 5:
         a= False
